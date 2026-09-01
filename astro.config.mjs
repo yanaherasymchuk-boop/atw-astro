a8ba6064ae0a394aedb3aafed5d0296217d2a1bd
Yana Herasymchuk <yana.herasymchuk@gmail.com>
2026-09-01 11:58:50 -0700

    Add sitemap robots and custom 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://atwdetailing.com",
+
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        !page.endsWith("/thank-you/") &&
+        !page.endsWith("/get-in-touch/"),
+    }),
+  ],
+
   vite: {
     server: {
       watch: {
